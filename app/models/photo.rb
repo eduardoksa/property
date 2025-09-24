c787d704e1fba206c1d82cf184db26079d5a08d6
eduardoksa <eduardoksa@protonmail.ch>
2025-09-24 09:09:41 -0300

    add photo and property models

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :property
+  has_one_attached :file
+
+  validates :file, presence: true
+  validates :position, presence: true, numericality: { greater_than: 0 }
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,9 @@
 class Property < ApplicationRecord
+  has_many :photos, -> { order(:position) }, dependent: :destroy
+
+  validates :name, presence: true
+
+  def cover_photo
+    photos[2]
+  end
 end
