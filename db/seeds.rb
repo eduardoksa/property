f0ec19ee64137e993312785fa3bddf0b1410e7b6
eduardoksa <eduardoksa@protonmail.ch>
2025-09-24 09:10:27 -0300

    add seeds to photo and property

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "faker"
+
+Property.destroy_all
+
+50.times do
+  property = Property.create!(name: Faker::Address.unique.community)
+
+  rand(3..5).times do |i|
+    property.photos.create!(
+      file: Rack::Test::UploadedFile.new(
+        Rails.root.join("spec/fixtures/files/sample#{rand(1..4)}.jpg")
+      ),
+      position: i + 1
+    )
+  end
+end
